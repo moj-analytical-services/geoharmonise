658d5759f4c051b1db7b1b08ab3a5aeab8547f66
phillip hall <philip.hall@justice.gsi.gov.uk>
2020-12-10 18:59:47 +0000

    Modified error message to skip over if no date is entered.

--- R/ONS_geolist.R
+++ R/ONS_geolist.R
@@ -16,9 +16,11 @@ ONS_geolist <- function(area_type, date = NULL) {
   
   # Add error if date is not in correct format
   
+  if(is.null(date)){} 
+  else{
   if(is.na(as.Date(date, "%d-%b-%y"))) {
     stop("Date is not in the expected format DD-MMM-YY")
-  } 
+  }}
 
   # Change parameter names
 
